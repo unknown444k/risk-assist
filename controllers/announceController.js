236bea7d48a38e3d15ece6c12068c556a07910bb
IPPO381 <ippo381@Pro66.local>
2023-07-23 17:26:46 +0530

    third risk

--- controllers/announceController.js
+++ controllers/announceController.js
@@ -153,12 +153,6 @@ const getspecificannounce = async (req, res) => {
   }
 };
 
-let i = 1;
-
-for(i; i<=10; i++){
-console.log("git updated")
-}
-
 module.exports = {
   createAnnounce,
   getAnnounment,
